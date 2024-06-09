5812e077dcc5c3cc4c6dc0dc209d4ea2f473b3aa
LexilesR <lexilesrivas@gmail.com>
2024-06-08 23:36:19 -0400

    programacion

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//rutas
+const routeindex = require('./routes/index');
+app.use ('/', routeindex);
+
+
+//motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//archivos estaticos
+app.use(express.static(path.join(__dirname, '../public')));
+
+//error
+app.use((req, res, next) => {
+    res.senndFile(path.join(__dirname, '../public/html/error.html'));
+})
+
+//servidor
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+})
